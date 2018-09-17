e9f71b1942b3ab3bb03c3f03ba0b351430f7ab42
Hached Ahmed <hachedahmeddev@gmail.com>
2018-09-17 17:46:28 +0200

    Add html home page

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render 
 
 
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, 'home.html', {'hithere': 'This is me'})
 
 def eggs(request):
     return HttpResponse('<h1>Eggs are great!</h1>')
